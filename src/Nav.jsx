fd45a43fb31a0b7562e942049b1cb41b58051a21
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-26 21:20:32 +0300

    added jsx

---
+++ src/Nav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Nav() {
+    return (
+        <nav>
+            <img src="../public/images/logo.png" alt="" className="logo"/>
+        </nav>
+    )
+}
