9c426360c04b426640eee6ff8c9b4e3cb4d633f3
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-26 21:22:08 +0300

    added data file

---
+++ public/data.js
@@ -0,0 +1,29 @@
+export default  [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "../public/images/avatar-vangogh.jpg",
+        post: "../public/images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "../public/images/avatar-courbet.jpg",
+        post: "../public/images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "../public/images/avatar-ducreux.jpg",
+        post: "../public/images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
