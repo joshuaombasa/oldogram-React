c51f15d1184a364e72894757d2a9712c6c5ac029
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-26 21:52:03 +0300

    updated jsx

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 
 export default function Profile(props) {
-console.log(props)
+
 
     return (
         <div className="profile">
             <div className="profile--header">
-                <img src="../public/images/avatar-courbet.jpg" alt="" className="profile--pic" />
+                <img src={props.avatar} alt="" className="profile--pic" />
                 <div className="profile-data">
-                    <p className="user--name">Joshua Ombasa</p>
-                    <p className="location">Manga Nyamira</p>
+                    <p className="user--name">{props.name}</p>
+                    <p className="location">{props.location}</p>
                 </div>
             </div>
-            <img src="../public/images/avatar-courbet.jpg" alt="" className="post-pic" />
+            <img src={props.post} alt="" className="post-pic" />
             <div className="socials">
                 <img src="../public/images/icon-heart.png" alt="" className="icons" />
                 <img src="../public/images/icon-comment.png" alt="" className="icons" />
                 <img src="../public/images/icon-dm.png" alt="" className="icons" />
             </div>
-            <p className="likes">21 likes</p>
-            <p className="comment">I just learnt React.</p>
+            <p className="likes">{props.likes} likes</p>
+            <p className="comment">{props.comment}</p>
         </div>
 
     )
