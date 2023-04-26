570be6e160142f07a7032af5ba5f0c85baeae354
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-26 22:16:09 +0300

    updated jsx

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -6,20 +6,20 @@ export default function Profile(props) {
     return (
         <div className="profile">
             <div className="profile--header">
-                <img src={props.avatar} alt="" className="profile--pic" />
+                <img src={props.item.avatar} alt="" className="profile--pic" />
                 <div className="profile-data">
-                    <p className="user--name"><strong>{props.name}</strong></p>
-                    <p className="location">{props.location}</p>
+                    <p className="user--name"><strong>{props.item.name}</strong></p>
+                    <p className="location">{props.item.location}</p>
                 </div>
             </div>
-            <img src={props.post} alt="" className="post-pic" />
+            <img src={props.item.post} alt="" className="post-pic" />
             <div className="socials">
                 <img src="../public/images/icon-heart.png" alt="" className="icons" />
                 <img src="../public/images/icon-comment.png" alt="" className="icons" />
                 <img src="../public/images/icon-dm.png" alt="" className="icons" />
             </div>
-            <p className="likes">{props.likes} likes</p>
-            <p className="comment"><strong>{props.username}</strong>  {props.comment}</p>
+            <p className="likes">{props.item.likes} likes</p>
+            <p className="comment"><strong>{props.item.username}</strong>  {props.item.comment}</p>
         </div>
 
     )
